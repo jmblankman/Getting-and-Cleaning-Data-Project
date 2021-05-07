8b7728b819c358b536c3bcb91dbc938a83abb8f6
jmblankman <58743893+jmblankman@users.noreply.github.com>
2021-05-06 23:54:21 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,21 @@
 ## Merge training and test sets
+
 x <- rbind(x_train, x_test)
 y <- rbind(y_train, y_test)
 subject <- rbind(subject_train, subject_test)
 combinedData <- cbind(x, y, subject)
 
 ## Extract only the measurements on the mean and standard deviation for each measurement
+
 library(dplyr)
 cleanData <- select(combinedData, subject, code, contains("Mean"), contains("mean"), contains("std"))
 
 ## Use descriptive activity names to name the activities in the data set
+
 cleanData$code <- activity_labels[cleanData$code, 2]
 
 ## Appropriately label the data set with descriptive variable names
+
 names(cleanData)[1] <- "Subject"
 names(cleanData)[2] <- "Activity"
 names(cleanData) <- gsub("Acc", "Accelerometer", names(cleanData))
@@ -29,5 +33,6 @@ names(cleanData) <- gsub("gravity", "Gravity", names(cleanData))
 
 ## Create a second, independent tidy data set with the average of each variable 
 ## for each activity and each subject
+
 groupData <- group_by(cleanData, Activity, Subject)
 finalData <- summarise_all(groupData, funs(mean))
